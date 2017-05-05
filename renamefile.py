3bc8f4152040dc628bf2182e568d784c96d592b6
İlayda Gündoğdu <ilaydagundogdu95@gmail.com>
2017-05-05 19:01:27 +0300

    Update renamefile.py
    Add comments

--- renamefile.py
+++ renamefile.py
@@ -8,9 +8,9 @@ mypath="/home/compleng/Masaüstü/music/"
 onlyfiles =os.listdir(mypath)
 
 for filename in onlyfiles:
-   if filename.startswith("WWW.IZLEVIDEO.NET-"):
+   if filename.startswith("WWW.IZLEVIDEO.NET-"): #file names are WWW.IZLEVIDEO.NET-Singer-Song.mp3
         rename(os.path.join(mypath, filename), os.path.join(mypath, filename[18:]))
-
+	#now file names are Singer-Song.mp3
 	
 
 
